aae16b335d86b0f625db45ce91ca99cee70b59bf
xiamingjie <15273155904@163.com>
2022-05-06 21:42:48 +0800

    update

--- concurrent_test.cc
+++ concurrent_test.cc
@@ -10,13 +10,13 @@ using namespace std;
 
 int main(int argc, char *argv[]) {
     if (argc < 5) {
-        printf("please input [host] [port] [threadNum] [preThreadConnNum].\n");
+        printf("please input [host] [port].\n");
         return 1;
     }
     const char *host = argv[1];
     int port = atoi(argv[2]);
-    int thread_number = atoi(argv[3]);
-    int per_conn_number = atoi(argv[4]);
+    int thread_number = 1000;
+    int per_conn_number = 1000;
 
     vector<thread> threads;
 
@@ -27,6 +27,9 @@ int main(int argc, char *argv[]) {
             for (int j = 0; j < per_conn_number; ++j) {
                 int connfd = fdwrapper::connect(host, port);
                 if (connfd < 0) continue;
+                if (j % 50 == 0) {
+                    this_thread::sleep_for(chrono::seconds(1));
+                }
             }
         });
     }
